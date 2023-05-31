d4467244a58ff4006081c5b1cea664a8bfca0f4c
Vamshi568 <kyadarivamshi123@gmail.com>
2023-05-31 16:31:27 +0530

    fixing error

--- app/layout.js
+++ app/layout.js
@@ -4,16 +4,11 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import { Providers } from './globalredux/provider';
 import AudioPlayer from '@/components/Playersmall';
-import Cookies from 'js-cookie';
 import { usePathname } from 'next/navigation';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'Melomix',
-  description: 'A Spotify clone developed with Next.js and powered by the Spotify Web API. Discover and stream your favorite music, create playlists, and explore a vast library of songs.',
 
-};
 
 export default function RootLayout({ children }) {
   
@@ -22,6 +17,7 @@ const url=usePathname()
     <html lang="en">
       <head>
       <title>Melomix</title>
+      <meta name="description" content="A Spotify clone developed with Next.js and powered by the Spotify Web API. Discover and stream your favorite music, create playlists, and explore a vast library of songs."/>
       <link rel="icon" href="/Frame.svg" type="image/svg+xml" sizes="any"/>
       </head>
       <body className={inter.className}>
